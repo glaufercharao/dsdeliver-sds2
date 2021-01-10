e4280dc6556c652e1f06925d0b3dba327da1dd3b
Gláufer Charão <glaufer.charao@gmail.com>
2021-01-09 21:47:41 -0300

    Add Home Page

---
+++ front-web/src/Home/index.tsx
@@ -0,0 +1,23 @@
+import './styles.css'
+import { ReactComponent as HomeImage} from '../assets/images/home.svg';
+const Home = () =>{
+    return(
+        <div className="home-container">
+            <div className="home-content">
+                <div className="home-actions">
+                    <h1 className="home-title">Faça seu pedido<br/> que entregamos<br/> para você!!!</h1>
+                    <h3 className="home-sub-title">
+                        Escolha o seu pedido que em poucos minutos<br /> 
+                        levamos para você
+                    </h3>
+                    <a href="order" className="home-btn-order">FAZER PEDIDO</a>
+                </div>
+                <div className="home-image">
+                    <HomeImage />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
