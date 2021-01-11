857281044f4903344a351cff9aa71e0fb83474bc
Gláufer Charão <glaufer.charao@gmail.com>
2021-01-11 00:20:49 -0300

    Finish project mobile

--- front-mobile/src/Orders/index.tsx
+++ front-mobile/src/Orders/index.tsx
@@ -1,23 +1,55 @@
-import React from 'react';
-import { StyleSheet, View} from 'react-native';
+import { useIsFocused, useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
+import { Alert, StyleSheet, View, Text } from 'react-native';
+import { ScrollView, TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { fetchOrders } from '../api';
 import Header from '../Header';
 import OrderCard from '../OrderCard'
+import { Order } from '../types';
 
 export default function Orders() {
+    const [orders, setOrders] = useState<Order[]>([])
+    const [isLoading, setIsLoading] = useState(false);
+    const navigation = useNavigation();
+    const isFocused = useIsFocused();
+
+    const fetchData = () => {
+        setIsLoading(true);
+        fetchOrders()
+            .then(response => setOrders(response.data))
+            .catch(() => Alert.alert('Houve um erro ao buscar os pedidos!'))
+            .finally(() => setIsLoading(false));
+    }
+
+    useEffect(() => {
+        if (isFocused) {
+            fetchData();
+        }
+    }, [isFocused]);
+
+    const handleOnPress = (order: Order) => {
+        navigation.navigate('OrderDetails', { order });
+    }
 
     return (
         <>
             <Header />
-            <View style={styles.container}>
-                <OrderCard />
-            </View>
+            <ScrollView style={styles.container}>
+                {isLoading ? (<Text>Buscando pedidos...</Text>) : (
+                    orders.map(order => (
+                        <TouchableWithoutFeedback key={order.id} onPress={() => handleOnPress(order)}>
+                            <OrderCard order={order} />
+                        </TouchableWithoutFeedback>
+                    ))
+                )}
+            </ScrollView>
         </>
     );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        paddingRight:'5%',
+    container: {
+        paddingRight: '5%',
         paddingLeft: '5%'
     }
 });
